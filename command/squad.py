0a0aa11cbe76b2c691602a78d7fad84d59f603ec
seigo2016 <2003seigo@gmail.com>
2019-12-10 15:33:18 +0900

    change manage platoon system

--- command/squad.py
+++ command/squad.py
@@ -31,8 +31,8 @@ chat = {"platoon_textchat_1": "",
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    print(before)
-    print(after)
+    if "platoon_lobby" in after.channel.name:
+        print(member)
 
 
 @client.event
