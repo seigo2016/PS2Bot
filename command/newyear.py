4985affd405a3b01ec133c057de362ece81a7d39
seigo2016 <2003seigo@gmail.com>
2019-12-31 23:31:06 +0900

    add newyear msg

--- command/newyear.py
+++ command/newyear.py
@@ -17,6 +17,7 @@ if not len(token):
 @tasks.loop(seconds=1)
 async def newyear():
     now = datetime.now().strftime('%H:%M:%S')
+    print(now)
     if now == '00:00:00' or now == '00:00:01':
         body = "あけましておめでとうございます。\n 今年もPlanetSide2JPコミュニティを宜しくお願いします"
         await client.get_guild(344369434103906314).get_channel(385401705975513093).send(body)
