8dfc296f3e645c0b054b7ccc26f249e9be81a8f4
seigo2016 <2003seigo@gmail.com>
2020-10-07 23:02:42 +0900

    role

--- func/bot.py
+++ func/bot.py
@@ -40,48 +40,14 @@ class JoinMember(commands.Cog):
 
     @commands.command()
     async def now(self, ctx, *, member: discord.Member = None):
-        readme_channel = self.bot.get_guild(server_id).get_channel(readme_channel_id)
-        print(readme_channel)
-        jst = timezone('Japan')
         utc = timezone('UTC')
         pst = timezone('America/Los_Angeles')
-        nowTimeJST = datetime.datetime.now()
-        nowTimePST = nowTimeJST.astimezone(pst)
-        nowTimeUTC = nowTimeJST.astimezone(utc)
-        body = (":flag_jp:(JP)JDT   {}\n:flag_us:(US)PST  {}\n:flag_gb:(UK)UTC {}")\
-            .format(nowTimeJST.strftime('%X'), nowTimePST.strftime('%X'), nowTimeUTC.strftime('%X'))
+        now_time_jst = datetime.datetime.now()
+        now_time_pst = now_time_jst.astimezone(pst)
+        now_time_utc = now_time_jst.astimezone(utc)
+        body = (":flag_jp:(JP)JST   {}\n:flag_us:(US)PST  {}\n:flag_gb:(UK)UTC {}")\
+            .format(now_time_jst.strftime('%X'), now_time_pst.strftime('%X'), now_time_utc.strftime('%X'))
         await ctx.send(body)
 
 def setup(bot):
     bot.add_cog(JoinMember(bot))
-
-# @client.event
-# async def on_message(message):
-#     jst = timezone('Japan')
-#     utc = timezone('UTC')
-#     pst = timezone('America/Los_Angeles')
-#     if message.content.startswith('!Help'):
-#         with open(current_dir+"/../message_template/help.txt", "r") as f:
-#             data = f.read()
-#         color = discord.Color.blue()
-#         em = discord.Embed(title='Help', description="\n\n" + data, colour=color)
-#         await message.channel.send(embed=em)
-#     elif message.content.startswith('J2PST') or message.content.startswith('P2JST'):
-#         try:
-#             commandText = message.content.split()
-#             today = date.today().strftime('%Y/%m/%d')
-#             strCurrentTime = commandText[1]
-#             if message.content.startswith('J2PST'):
-#                 currentTimeJST = dt.strptime(today + ' ' + strCurrentTime, '%Y/%m/%d %H:%M:%S')
-#                 currentTimeUTC = currentTimeJST.astimezone(utc)
-#                 currentTimePST = currentTimeUTC.astimezone(pst)
-#                 resultTime = currentTimePST
-#             elif message.content.startswith('P2JST'):
-#                 currentTimePST = dt.strptime(today + ' ' + strCurrentTime, '%Y/%m/%d %H:%M:%S')
-#                 currentTimeUTC = currentTimePST.astimezone(utc)
-#                 currentTimeJST = currentTimeUTC.astimezone(jst)
-#                 resultTime = currentTimeJST
-#             await message.channel.send(resultTime.strftime('%H:%M:%S'))
-#         except Exception as e:
-#             print(e)
-#             message.channel.send("正しい値を入力してください")
--- func/role.py
+++ func/role.py
@@ -47,43 +47,44 @@ class ManageRole(commands.Cog):
         self.emoji_vs_id = 384317750593585152
         self.emoji_ns_id = 653944468356988938
         self.emoji_jpc_id = 701436271410544660
-        if not payload.member.bot:
-            if payload.message_id == message_id:
-                rolelist = {}
-                # self.emoji_role = {self.emoji_jpc_id: 'Mercenary', self.emoji_nc_id: 'MainNC', self.emoji_tr_id: 'MainTR', self.emoji_vs_id: 'MainVS', self.emoji_ns_id: 'NS', '🟦': 'NC', '🟥': 'TR', '🟪': 'VS',
-                #     '1️⃣': 'Soltech', '2️⃣': 'Connery', '3️⃣': 'Emerald', '4️⃣': 'Miller'}
-                for role_name in self.emoji_role.values():
-                    rolelist[role_name] = discord.utils.get(self.bot.get_guild(server_id).roles, name=role_name)
-                self.emoji_name = payload.emoji.name
-                self.emoji_id = payload.emoji.id
-                if self.emoji_id in self.emoji_role:
-                    select_role = rolelist[self.emoji_role[self.emoji_id]]
-                    if select_role in payload.member.roles:
-                        await payload.member.remove_roles(select_role)
-                        body = f"`{payload.member}` さんの  `{select_role}` 役職を削除しました \n(このメッセージは一定時間で消去されます)"
-                    else:
-                        await payload.member.add_roles(select_role)
-                        body = f"`{payload.member}` さんに  `{select_role}` 役職を追加しました \n(このメッセージは一定時間で消去されます)"
-                    await self.fixed_message.remove_reaction(payload.emoji, payload.member)
-                    reply_message = await self.bot.get_guild(server_id).get_channel(role_channel_id).send(body)
-                    await asyncio.sleep(30)
-                    await reply_message.delete()
-                elif self.emoji_name in self.emoji_role:
-                    select_role = rolelist[self.emoji_role[self.emoji_name]]
-                    if select_role in payload.member.roles:
-                        await payload.member.remove_roles(select_role)
-                        body = f"`{payload.member}` さんの  `{select_role}` 役職を削除しました \n(このメッセージは一定時間で消去されます)"
-                    else:
-                        await payload.member.add_roles(select_role)
-                        body = f"`{payload.member}` さんに  `{select_role}` 役職を追加しました \n(このメッセージは一定時間で消去されます)"
-                    emoji = discord.utils.get(self.bot.get_guild(server_id).emojis, name=self.emoji_name)
-                    if emoji:
-                        await self.fixed_message.remove_reaction(emoji, payload.member)
-                    else:
-                        await self.fixed_message.remove_reaction(self.emoji_name, payload.member)
-                    reply_message = await self.bot.get_guild(server_id).get_channel(role_channel_id).send(body)
-                    await asyncio.sleep(30)
-                    await reply_message.delete()
+        if payload.member.bot:
+            pass
+        if payload.message_id == message_id:
+            rolelist = {}
+            # self.emoji_role = {self.emoji_jpc_id: 'Mercenary', self.emoji_nc_id: 'MainNC', self.emoji_tr_id: 'MainTR', self.emoji_vs_id: 'MainVS', self.emoji_ns_id: 'NS', '🟦': 'NC', '🟥': 'TR', '🟪': 'VS',
+            #     '1️⃣': 'Soltech', '2️⃣': 'Connery', '3️⃣': 'Emerald', '4️⃣': 'Miller'}
+            for role_name in self.emoji_role.values():
+                rolelist[role_name] = discord.utils.get(self.bot.get_guild(server_id).roles, name=role_name)
+            self.emoji_name = payload.emoji.name
+            self.emoji_id = payload.emoji.id
+            if self.emoji_id in self.emoji_role:
+                select_role = rolelist[self.emoji_role[self.emoji_id]]
+                if select_role in payload.member.roles:
+                    await payload.member.remove_roles(select_role)
+                    body = f"`{payload.member}` さんの  `{select_role}` 役職を削除しました \n(このメッセージは一定時間で消去されます)"
+                else:
+                    await payload.member.add_roles(select_role)
+                    body = f"`{payload.member}` さんに  `{select_role}` 役職を追加しました \n(このメッセージは一定時間で消去されます)"
+                await self.fixed_message.remove_reaction(payload.emoji, payload.member)
+                reply_message = await self.bot.get_guild(server_id).get_channel(role_channel_id).send(body)
+                await asyncio.sleep(30)
+                await reply_message.delete()
+            elif self.emoji_name in self.emoji_role:
+                select_role = rolelist[self.emoji_role[self.emoji_name]]
+                if select_role in payload.member.roles:
+                    await payload.member.remove_roles(select_role)
+                    body = f"`{payload.member}` さんの  `{select_role}` 役職を削除しました \n(このメッセージは一定時間で消去されます)"
+                else:
+                    await payload.member.add_roles(select_role)
+                    body = f"`{payload.member}` さんに  `{select_role}` 役職を追加しました \n(このメッセージは一定時間で消去されます)"
+                emoji = discord.utils.get(self.bot.get_guild(server_id).emojis, name=self.emoji_name)
+                if emoji:
+                    await self.fixed_message.remove_reaction(emoji, payload.member)
+                else:
+                    await self.fixed_message.remove_reaction(self.emoji_name, payload.member)
+                reply_message = await self.bot.get_guild(server_id).get_channel(role_channel_id).send(body)
+                await asyncio.sleep(30)
+                await reply_message.delete()
 
 def setup(bot):
     bot.add_cog(ManageRole(bot))
