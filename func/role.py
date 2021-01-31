4e38bb926ba3b5153c5a81e842833c8db1c7b718
seigo2016 <2003seigo@gmail.com>
2021-01-31 09:06:20 +0900

    開発環境用の設定を追加

--- func/role.py
+++ func/role.py
@@ -1,24 +1,24 @@
 # coding:utf-8
 from discord.ext import commands
 import discord
-
 import configparser
 import os
-
 import time
 import asyncio
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-config = configparser.ConfigParser()
-config.read(current_dir + "/../config.ini")
-server_id = int(config['Server']['Server_ID'])
-role_channel_id = int(config['Channel']['Role_Channel_ID'])
-message_id = int(config['Message']['Role_Message_ID'])
-
 class ManageRole(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, env):
         self.bot = bot
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        config = configparser.ConfigParser()
+        if env == "dev":
+            config.read(current_dir + "/../config-dev.ini")
+        else:
+            config.read(current_dir + "/../config.ini")
+        self.server_id = int(config['Server']['Server_ID'])
+        self.role_channel_id = int(config['Channel']['Role_Channel_ID'])
+        self.message_id = int(config['Message']['Role_Message_ID'])
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -31,7 +31,7 @@ class ManageRole(commands.Cog):
             emoji_main_jpc: 'Mercenary', emoji_main_nc: 'MainNC', emoji_main_tr: 'MainTR', emoji_main_vs: 'MainVS',\
             emoji_main_ns: 'NS', '🟦': 'NC', '🟥': 'TR', '🟪': 'VS',\
             '1️⃣': 'Soltech', '2️⃣': 'Connery', '3️⃣': 'Emerald', '4️⃣': 'Miller'}
-        self.fixed_message = await self.bot.get_guild(server_id).get_channel(role_channel_id).fetch_message(message_id)
+        self.fixed_message = await self.bot.get_guild(self.server_id).get_channel(self.role_channel_id).fetch_message(self.message_id)
         for emoji_name in self.emoji_role.keys():
             await self.fixed_message.add_reaction(emoji_name)
 
@@ -39,10 +39,10 @@ class ManageRole(commands.Cog):
     async def on_raw_reaction_add(self, payload):
         if payload.member.bot:
             pass
-        elif payload.message_id == message_id:
+        elif payload.message_id == self.message_id:
             role_list = {}
             for role_name in self.emoji_role.values():
-                role_list[role_name] = discord.utils.get(self.bot.get_guild(server_id).roles, name=role_name)
+                role_list[role_name] = discord.utils.get(self.bot.get_guild(self.server_id).roles, name=role_name)
             reaction_emoji = self.bot.get_emoji(payload.emoji.id)
             if reaction_emoji in self.emoji_role:
                 select_role = role_list[self.emoji_role[reaction_emoji]]
@@ -60,9 +60,9 @@ class ManageRole(commands.Cog):
                     await payload.member.add_roles(select_role)
                     body = f"`{payload.member}` さんに  `{select_role}` 役職を追加しました \n(このメッセージは一定時間で消去されます)"
                 await self.fixed_message.remove_reaction(payload.emoji, payload.member)
-                reply_message = await self.bot.get_guild(server_id).get_channel(role_channel_id).send(body)
+                reply_message = await self.bot.get_guild(self.server_id).get_channel(self.role_channel_id).send(body)
                 await asyncio.sleep(30)
                 await reply_message.delete()
 
-def setup(bot):
-    bot.add_cog(ManageRole(bot))
+def setup(bot, env):
+    bot.add_cog(ManageRole(bot, env))
