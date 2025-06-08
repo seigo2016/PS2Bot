4a8f1a1b3f0d60dd34018c62308fefc6e2f30d1d
seigo2016 <mail@seigo2016.com>
2025-06-08 23:08:27 +0900

    feat: アラート機能を復元

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 import argparse
 import func.role as role
 import func.squad as squad
+import func.alert as alert
 import func.bot as bot
 
 parser = argparse.ArgumentParser()
@@ -25,6 +26,7 @@ token = os.environ['token']
 role.setup(client, env)
 squad.setup(client, env)
 bot.setup(client, env)
+alert.setup(client, env)
 
 @client.event
 async def on_ready():
